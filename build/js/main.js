963cfd81ca75ba5f8ff1a21100eaf4ef758de4e8
Andrey <i3hanesy@yandex.ru>
2020-04-03 19:19:40 +0500

    создает вложенные комментарии

--- build/js/main.js
+++ build/js/main.js
@@ -9,12 +9,20 @@
   var nameField = form.querySelector('input[type="text"]');
   var mailField = form.querySelector('input[type="email"]');
   var textField = form.querySelector('textarea');
+
   var commentTemplate = document.querySelector('#new-comment').content.querySelector('li');
   var commentListTemplate = document.querySelector('#new-comment-list').content.querySelector('ul');
+  var formTemplate = document.querySelector('#new-form').content.querySelector('form');
 
   var fragment = document.createDocumentFragment();
   var insertElements = function (locationOfInsertion) {
     locationOfInsertion.appendChild(fragment);
+  };
+
+  // создает внутренний список
+  var getAnswerList = function (location) {
+    fragment.appendChild(commentListTemplate.cloneNode(true));
+    insertElements(location);
   };
 
   // чистит форму
@@ -24,29 +32,58 @@
     textField.value = '';
   };
 
-  var getAnswerList = function () {
-    if (comments.querySelector('.comments__list-inner--one')) {
-      return;
-    }
-
-    fragment.appendChild(commentListTemplate.cloneNode(true));
-    insertElements(firstCommentsList);
+  var getNewForm = function (location) {
+    fragment.appendChild(formTemplate.cloneNode(true));
+    insertElements(location);
   };
 
+  // создает комментарий по шаблону
   var getComment = function (userName, userText) {
     var element = commentTemplate.cloneNode(true);
     element.querySelector('.comment__user-name').textContent = userName;
     element.querySelector('.comment__description p').textContent = userText;
     fragment.appendChild(element);
 
-    insertElements(firstCommentsList);
+    // кнопка ответить
+    var commentAnswerButton = element.querySelector('.comment__answer');
+    commentAnswerButton.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      form.removeEventListener('submit', onSubmitFormDown);
+
+      // обработчик внутренних комментариев
+      var onSubmitFormDown2 = function (event) {
+        event.preventDefault();
+
+        getComment(nameField.value, textField.value);
+        insertElements(element.querySelector('.comments__list-inner'));
+        getClearForm();
+        form.removeEventListener('submit', onSubmitFormDown2);
+        form.addEventListener('submit', onSubmitFormDown);
+      };
+
+      // проверка на наличие списка
+      if (!element.querySelector('.comments__list-inner')) {
+        getAnswerList(element);
+        form.addEventListener('submit', onSubmitFormDown2);
+      } else {
+        form.addEventListener('submit', onSubmitFormDown2);
+      }
+    });
   };
 
-  form.addEventListener('submit', function (evt) {
+  // обработчик комментариев первого уровня
+  var onSubmitFormDown = function (evt) {
     evt.preventDefault();
 
     getComment(nameField.value, textField.value);
+    insertElements(firstCommentsList);
     getClearForm();
-  });
+  };
+
+  form.addEventListener('submit', onSubmitFormDown);
+
+  document.addEventListener('DOMContentLoaded', function () {
+    getNewForm(comments);
+  }, false);
 
 })();
